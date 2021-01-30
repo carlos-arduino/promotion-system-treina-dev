ee904d649a8d407df0e818b75f12642407ec2397
carlos-arduino <cae_arduino@icloud.com>
2021-01-30 16:49:18 -0300

    create promotion with every fields and add I18n

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,2 +1,4 @@
 class Promotion < ApplicationRecord
+    validates :name, :code, :discount_rate, :coupon_quantity, :expiration_date, presence: true
+    validates :code , uniqueness: { message: "deve ser único" } 
 end
